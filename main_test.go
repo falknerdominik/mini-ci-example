31f65db4034185e1f31f74d7d530591fe458aea2
Dominik Falkner <falkner.dominik@gmail.com>
2019-12-09 14:42:29 +0100

    Add error

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestGetMinuteUp(t *testing.T) {
-
+	t.Error("getMinute(1, 30) = d; wanted 2")
 	res := getMinute(1, 30)
 	if res != 2 {
 		t.Errorf("getMinute(1, 30) = %d; wanted 2", res)
